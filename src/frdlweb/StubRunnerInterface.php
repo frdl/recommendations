6364ca52c32c22ac46c2f54d8b397b1c1032a0d9
Till Wehowski <github@webfan.de>
2020-05-14 09:22:46 +0200

    Create StubRunnerInterface.php

---
+++ src/frdlweb/StubRunnerInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace frdlweb;
+
+interface StubRunnerInterface
+ { 
+ 	public function loginRootUser($username = null, $password = null) : bool;		
+	public function isRootUser() : bool;
+	public function getStubVM() : StubHelperInterface;	
+	public function getStub() : StubItemInterface;		
+	public function __invoke() :?StubHelperInterface;	
+	public function getInvoker();	
+	public function getShield();	
+	public function autoloading() : void;
+}
