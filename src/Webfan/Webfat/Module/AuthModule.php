69170e32e5007833b39460b36edbb8d0b8dc5142
Till Wehowski <github@webfan.de>
2022-11-26 18:35:54 +0100

    Create AuthModule.php

---
+++ src/Webfan/Webfat/Module/AuthModule.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Webfan\Webfat\Module;
+
+use namespace Webfan\Webfat\Intent\IntentInterface;
+use Webfan\Webfat\Module as BaseModule;
+use Webfan\Webfat\Module\ModuleInterface;
+
+class AuthModule extends BaseModule implements IntentInterface
+{
+  protected $name = 'main';
+}
