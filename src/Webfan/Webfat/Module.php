f0ee0008a505cefd631d2401c894d7b84b4635f8
Till Wehowski <github@webfan.de>
2022-12-04 16:38:41 +0100

    Update Module.php

--- src/Webfan/Webfat/Module.php
+++ src/Webfan/Webfat/Module.php
@@ -6,6 +6,8 @@ use Webfan\Webfat\Module\ModuleInterface;
 
 abstract class Module implements ModuleInterface
 {
+   use Webfan\ngModuleTrait;
+   
    public function getName():string
    {
       return $this->name; 
