f303da212a0b4816b1e6d910ec5f294466baa45c
Till Wehowski <github@webfan.de>
2022-12-10 10:07:40 +0100

    Update Module.php

--- src/Webfan/Webfat/Module.php
+++ src/Webfan/Webfat/Module.php
@@ -6,7 +6,33 @@ use Webfan\Webfat\Module\ModuleInterface;
 
 abstract class Module implements ModuleInterface
 {
-   use \Webfan\ngModuleTrait;
+   use \Webfan\ngModuleTrait, 
+   \Webfan\setupModuleTrait;
+   
+   public function _niy($method)
+   {
+       throw new \Exception(sprintf('You mus implement and overwrite the method %1$s in the class %2$s!', $method, get_class($this)));
+   }
+   
+   public function install(array $args = [], string $section = null)
+   {
+      $this->_niy(__METHOD__);
+   }
+   
+	public function uninstall(array $args = [], string $section = null)
+   {
+      $this->_niy(__METHOD__);
+   }
+   
+	public function build(array $args = [], string $section = null)
+   {
+      $this->_niy(__METHOD__);
+   }
+   
+	public function getContainer() : ?ContainerInterface
+   {
+      $this->_niy(__METHOD__);
+   }
    
    public function getName():string
    {
