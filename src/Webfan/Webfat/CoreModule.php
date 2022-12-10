2655e7344b6c599c43b9bdef0e973940c4e717d1
Till Wehowski <github@webfan.de>
2022-12-10 16:34:42 +0100

    Create CoreModule.php

---
+++ src/Webfan/Webfat/CoreModule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Webfan\Webfat;
+
+use Webfan\Webfat\App\GenericModuleCore;
+
+use Webfan\Webfat\Intent\IntentInterface;
+use Webfan\Webfat\Module as BaseModule;
+use Webfan\Webfat\Module\ModuleInterface;
+
+
+final class CoreModule extends GenericModuleCore implements ModuleInterface, IntentInterface
+{
+  
+}
